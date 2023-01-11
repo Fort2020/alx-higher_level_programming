bb52c2bf4e4a56fa5eba8f87e9cc2b83c4517ae0
fort2020 <frankfortdata@gmail.com>
2023-01-11 18:34:19 +0000

    Fixed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 def search_replace(my_list, search, replace):
     if not my_list:
         print()
-        return [find if find != search else replace for find in my_list]
+    return [find if find != search else replace for find in my_list]
