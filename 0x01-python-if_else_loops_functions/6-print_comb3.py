04743e4d200ece1597f55a91912b6746efe131e3
fort2020 <frankfortdata@gmail.com>
2023-01-17 12:31:18 +0000

    Fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for tens in range(0, 10):
-    for ones in range(tens + 1, 10):
-        print("{}{}, ".format(tens, ones), end='')
-print("89")
+for i in range(10):
+    for n in range(i+1,10):
+        if i == 8:
+            print("{}{}".format(i, n))
+        else:
+            print("{}{}".format(i, n), end="")
