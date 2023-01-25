9f270de6b50048952bd04b571001649aa584971d
fort2020 <frankfortdata@gmail.com>
2023-01-25 16:42:50 +0000

    Updated

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    s = 0
+    for s in range(x):
+        try:
+            print(my_list[m], end="")
+            s += 1
+        except IndexError:
+            break
+    print()
+    return n
