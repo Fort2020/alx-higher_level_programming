e50e4735c7e44492fab20e03d11dd9fa502ff71d
fort2020 <frankfortdata@gmail.com>
2023-03-15 16:44:10 -0700

    All Tasks

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- a script that creates first_table in the current database in your MySQL server.
+CREATE TABLE IF NOT EXISTS first_table (id INT,
+name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- a script that print table first_table from the database hbtn_0c_0 in your MySQL server.
+SHOW CREATE TABLE 'first_table';
