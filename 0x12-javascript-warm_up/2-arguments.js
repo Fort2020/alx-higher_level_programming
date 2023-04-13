99787d4c82f0de81883ed3ef9a95e5f9f8df7639
fort2020 <frankfortdata@gmail.com>
2023-04-13 10:03:20 -0700

    Task 2. Updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // If no arguments, print "No argument", if 1 found print "Argument found"
 // Otherwise print "Arguments Found"
 if (process.argv.length === 2) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
-console.log('Arguments found');
+  console.log('Arguments found');
 }
