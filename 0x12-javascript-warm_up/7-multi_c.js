2dc3b3b1d5bdce77d87411022704786e168e1484
fort2020 <frankfortdata@gmail.com>
2023-04-13 10:58:57 -0700

    Task 7.

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,17 +1,13 @@
 #!/usr/bin/node
-//A script that prints x times "C is fun"
-//Where x is the first argument of the script.
-
-//If the first argument can’t be converted to an integer,
-//print "Missing number of occurrences"
-
-	const myArgs = process.argv.slice(2);
-	let n = 0;
-	if (isNaN(myArgs[0])) {
-	  console.log("Missing number of occurrences");
-	} else {
-	  while (n < parseInt(myArgs[0])) {
-	    console.log("C is fun");
-	    n++;
-	  }
-	}
+// Prints x times "C is fun", Where x is first argument of the script.
+// If it can’t be converted to an integer, print "Missing number of occurrences"
+const myArgs = process.argv.slice(2);
+let n = 0;
+if (isNaN(myArgs[0])) {
+  console.log("Missing number of occurrences");
+} else {
+  while (n < parseInt(myArgs[0])) {
+    console.log("C is fun");
+    n++;
+  }
+}
