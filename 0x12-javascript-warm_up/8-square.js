8f44c820f4019ca8126e3587b4e2a0b5c77779a4
fort2020 <frankfortdata@gmail.com>
2023-04-13 11:10:13 -0700

    Task 8.

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,19 +1,16 @@
 #!/usr/bin/node
-//A script that prints a square
-//The first argument is the size of the square
-
-//If the first argument can’t be converted to an integer, print "Missing size"
-
-	const myArgs = process.argv.slice(2);
-	let a, b;
-	if (isNaN(myArgs[0])) {
-	  console.log("Missing size");
-	} else {
-	  for (a = 0; a < parseInt(myArgs[0]); a++) {
-	    let row = "";
-	    for (b = 0; b < parseInt(myArgs[0]); b++) {
-	      row += "X";
-	    }
-	    console.log(row);
-	  }
-	}
+// Prints a square, first argument is the size of the square
+// If it can't be converted to an integer, print "Missing size"
+const myArgs = process.argv.slice(2);
+let a, b;
+if (isNaN(myArgs[0])) {
+  console.log("Missing size");
+} else {
+  for (a = 0; a < parseInt(myArgs[0]); a++) {
+    let row = "";
+    for (b = 0; b < parseInt(myArgs[0]); b++) {
+      row += "X";
+    }
+    console.log(row);
+  }
+}
