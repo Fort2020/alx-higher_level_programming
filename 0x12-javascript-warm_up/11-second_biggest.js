1daa6b5fbc4d4b7c4c29ba11c3474b21f6c00814
fort2020 <frankfortdata@gmail.com>
2023-04-13 11:33:17 -0700

    Task 11. done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// Searching 2 biggest integer in argument, if none, print 0: if arguments is 1, print 0
 const myArgs = process.argv.slice(2);
 if (!myArgs[0] || !myArgs[2]) {
   console.log(0);
