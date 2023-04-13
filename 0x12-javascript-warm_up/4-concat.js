45a91d1c99a2d20f6e804b3cfa015f99ea18b6f3
fort2020 <frankfortdata@gmail.com>
2023-04-13 10:11:04 -0700

    Task 4. Fixed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,13 +1,10 @@
-Task 4. 4-concat.js
--------------------
 #!/usr/bin/node
-//A script that prints two arguments passed to it, in the following format: " is "
-
-	const myArgs = process.argv.slice(2);
-	console.log(myArgs[0], "is", myArgs[1]);
-	if (!myArgs[0]) {
-	    console.log("undefined is undefined");
-	}
-	else {
-	    console.log(myArgs[0], "is", myArgs[1]);
-	}
+// This script prints 2 arguments in the following format: " is "
+const myArgs = process.argv.slice(2);
+console.log(myArgs[0], 'is', myArgs[1]);
+if (!myArgs[0]) {
+    console.log("undefined is undefined");
+}
+else {
+    console.log(myArgs[0], 'is', myArgs[1]);
+}
