b02975da9db4b0ce8950551384f82d925cab5442
fort2020 <frankfortdata@gmail.com>
2023-04-13 10:52:16 -0700

    Fixing

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-//A script that prints 3 lines, but by using an array of string and a loop
-
-	const myLines = ["C is fun", "Python is cool", "JavaScript is amazing"];
-	const arrayLength = myLines.length;
-	let n = 0;
-	while (i < arrayLength) {
-	  console.log(myLines[n]);
-	  n++;
-	}
+// A script that prints 3 lines, but by using an array of string and a loop
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
+}
