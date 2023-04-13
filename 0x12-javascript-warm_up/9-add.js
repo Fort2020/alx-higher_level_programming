5ef4e2ca9a7490e727546ca95dd1afeb2d55cfa6
fort2020 <frankfortdata@gmail.com>
2023-04-13 11:15:23 -0700

    Task 9.

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-//A script that prints the addition of 2 integers
-
-//First argument, first integer: second argument, second integer
-//Define a function with this prototype: function add(a, b)
-
-	const myArgs = process.argv.slice(2);
-	let result = 0;
-	function add (a, b) {
-	  result = a + b;
-	  return result;
-	}
-	add(parseInt(myArgs[0]), parseInt(myArgs[1]));
-	console.log(result);
+// Prints addition of 2 integers, 1 argument, 1 integer: 2 argument, 2 integer
+// Define a function with this prototype: function add(a, b)
+const myArgs = process.argv.slice(2);
+let result = 0;
+function add (a, b) {
+  result = a + b;
+  return result;
+}
+add(parseInt(myArgs[0]), parseInt(myArgs[1]));
+console.log(result);
