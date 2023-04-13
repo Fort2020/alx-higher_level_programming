6d8218bab65789d955c932b5af65287e34c479ee
fort2020 <frankfortdata@gmail.com>
2023-04-13 09:36:50 -0700

    Updated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-//A script that prints 3 lines using Console
-
-	console.log("C is fun\n"
-		"Python is cool\n"
-		"JavaScript is amazing");
+// A script that prints 3 lines using Console
+console.log("C is fun\n"
+"Python is cool\n"
+"JavaScript is amazing");
