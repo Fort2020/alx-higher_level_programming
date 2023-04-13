826350a7d775fe95b2c62df5185e066ec04c633c
fort2020 <frankfortdata@gmail.com>
2023-04-13 11:22:40 -0700

    Task 10 done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
-//A script that computes and prints a factorial.
-
-//Factorial of NaN is 1
-
-	const myArgs = process.argv.slice(2);
-	function factorial (num) {
-	  if (isNaN(myArgs[0])) {
-	    return 1;
-	  }
-	  if (num <= 1) {
-	    return 1;
-	  }
-	  return num * factorial(num - 1);
-	}
-	console.log(factorial(parseInt(myArgs[0])));
+// This script computes and prints a factorial: of NaN is 1
+const myArgs = process.argv.slice(2);
+function factorial (num) {
+  if (isNaN(myArgs[0])) {
+    return 1;
+  }
+  if (num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+console.log(factorial(parseInt(myArgs[0])));
