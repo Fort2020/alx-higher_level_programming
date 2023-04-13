78d8dd51d44a376bbde361e7f81211e4d1260e75
fort2020 <frankfortdata@gmail.com>
2023-04-13 12:43:58 -0700

    Task 16.

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-//A function that increments and calls a function.
-
-	exports.addMeMaybe = function (number, theFunction) {
-	  number++;
-	  theFunction(number);
-	};
+// This function increments and calls a function.
+exports.addMeMaybe = function (number, theFunction) {
+  number++;
+  theFunction(number);
+};
