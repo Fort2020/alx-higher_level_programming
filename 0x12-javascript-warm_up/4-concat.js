f4e181caf7727b2f3d7a903a0c4392b6773f61a8
fort2020 <frankfortdata@gmail.com>
2023-04-13 10:23:43 -0700

    Updated

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
-if (!myArgs[0]) {
-    console.log('undefined is undefined');
-}
-else {
-    console.log(myArgs[0], 'is', myArgs[1]);
-}
+// This script prints 2 arguments in the following format: " is "
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
