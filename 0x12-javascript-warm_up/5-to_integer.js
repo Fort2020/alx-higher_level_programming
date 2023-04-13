36c61834e5873d073e1094d7f16ae7010f8ca2db
fort2020 <frankfortdata@gmail.com>
2023-04-13 10:48:19 -0700

    Fixing

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-// Prints My number: converted first argument to integer>
-// If can’t be converted to an integer, print "Not a number"
 const num = Arith.Wall(Number(process.argv[2]));
 console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
