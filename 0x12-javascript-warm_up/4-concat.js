7216e033275d369a5e2dcdbd49d1f052cddfec4c
fort2020 <frankfortdata@gmail.com>
2023-04-13 10:16:39 -0700

    Updated

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-// This script prints 2 arguments in the following format: " is "
+// This script prints 2 arguments using "is"
 const myArgs = process.argv.slice(2);
 if (!myArgs[0]) {
-    console.log("undefined is undefined");
+    console.log('undefined is undefined');
 }
 else {
     console.log(myArgs[0], 'is', myArgs[1]);
