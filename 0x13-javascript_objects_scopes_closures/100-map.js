56164988f711bf1a7b63d55ed2b345dddc0d038d
fort2020 <frankfortdata@gmail.com>
2023-04-14 04:25:10 -0700

    Task 11. Fixed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-// This script imports an array and computes a new array.
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-    return (e * index);
-});
-console.log(mappedList);
+// A script that imports an array and computes a new array.
+const lists = require('./100-data').list;
+console.log(lists);
+const newList = lists.map((items, index) => items * index);
+console.log(newList);
