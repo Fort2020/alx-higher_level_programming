f2e6df7dda723db0a442f5dea7c811da1fa2db8f
fort2020 <frankfortdata@gmail.com>
2023-04-14 04:27:44 -0700

    Updated Task 10.

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // Converts a number from base 10 to another base passed as argument 
 exports.converter = function (base) {
-  function convert (number) {
-    return number.toString(base);
-  }
-  return convert;
+   return function (num) {
+    return num.toString(base);
+  };
 };
