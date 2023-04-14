b6304d4c3904adbc4df2a6b55eca6cc0c9dd6293
fort2020 <frankfortdata@gmail.com>
2023-04-14 04:13:26 -0700

    Task 10. Fixed

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Converts a number from base 10 to another base passed as argument: 
+// Converts a number from base 10 to another base passed as argument 
 exports.converter = function (base) {
   return function (n) {
     return (n.toString(base));
