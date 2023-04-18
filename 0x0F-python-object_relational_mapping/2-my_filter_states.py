0359ea11b2d0b201acb16e4f68e13f241690a4f5
fort2020 <frankfortdata@gmail.com>
2023-04-18 01:51:10 -0700

    Task 2. Fixed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script takes in an argument and displays 
+"""This script takes in an argument and displays
 all values in the states table of hbtn_0e_0_usa
 """
 import MySQLdb
