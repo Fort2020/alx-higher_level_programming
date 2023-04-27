37316953d8c79e4a60ab895085e3b0f44661d49b
fort2020 <frankfortdata@gmail.com>
2023-04-27 15:34:07 -0700

    Fixed

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send JSON POST request to URL passed as first argument, shows the body of the response.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2") "$1"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
