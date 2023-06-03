075a21fba9b5fd57c8a092444493b4600f72942a
fort2020 <frankfort@gmail.com>
2023-06-03 08:06:45 -0700

    Fixed Task 3.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-/* 
- A script that prints the title of a Star Wars movie
- where the episode number matches a given integer
+/*
+ A script that prints the title of a Star Wars movie,
+ where the episode number matches a given integer.
 */
 
 const request = require('request');
