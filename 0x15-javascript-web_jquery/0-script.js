002909feb12f4758bd727877f6397a647ad2e891
fort2020 <frankfort@gmail.com>
2023-06-03 09:58:19 -0700

    Tired Saturday

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+// JavaScript script that updates the text color
+// of the <header> element to red (#FF0000)
+// Use document.querySelector to select the HTML tag
+
+const colorH = document.querySelector('header');
+colorH.style.color = '#FF0000';
